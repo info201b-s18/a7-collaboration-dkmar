b92cde88aba4891b6ad96b7855e46c97ea2c6cce
Chloe <cjl0707@uw.edu>
2018-05-09 14:16:57 -0700

    chart one updated v2

--- chart_one.R
+++ chart_one.R
@@ -10,7 +10,8 @@ survey_data <- read.csv("data/intro-survey.csv", as.is = TRUE)
 # wrangling with the data 
 survey_data_sorted <- survey_data %>%
   rename("class_standing" = What.is.your.current.class.standing.,
-         "coding_experience" = How.would.you.describe.your.coding.programming.experience.) %>%
+         "coding_experience" =
+           How.would.you.describe.your.coding.programming.experience.) %>%
   mutate(coding_experience = gsub( " .*$", "", coding_experience)) %>%
   replace(. == "", "Not Specified") %>%
   group_by(class_standing, coding_experience) %>%
@@ -22,23 +23,24 @@ survey_data_sorted <- survey_data %>%
 
 # Function called to draw the chart 
 chart_one_function <- function(data) {
-  # First factor class standing and experience level to rearrange the order of x-asix
+  # First factor class standing and experience level to rearrange
+  # the order of x-asix
   class <- factor(data$class_standing,
                   levels = c("Freshman", "Sophomore", "Junior", "Senior",
                              "Not Specified"))
   group <- factor(data$coding_experience,
                   levels = c("Not Specified", "Lots", "Moderate",
                              "Experimented", "Never"))
-  chart <- ggplot() + theme_bw() + 
+  chart <- ggplot() + theme_bw() +
     geom_bar(aes(y = n, x = class, fill = group),
              data = survey_data_sorted, stat = "identity") +
     scale_fill_manual(values = c("coral", "gold", "deepskyblue",
                                  "seagreen1", "pink1")) +
     geom_text(data = survey_data_sorted,
-              aes(x = class, y = n, label = paste0(n," students")),
+              aes(x = class, y = n, label = paste0(n, " students")),
               size = 3, position = position_stack(vjust = 0.5)) +
     theme(legend.position = "bottom",
-          legend.direction ="horizontal",
+          legend.direction = "horizontal",
           legend.title = element_text(size = 8),
           legend.background = element_rect(fill = "white",
                                            size = 0.5,
@@ -54,4 +56,3 @@ chart_one_function <- function(data) {
 
 # Testing the function
 chart_one_function(survey_data_sorted)
-
