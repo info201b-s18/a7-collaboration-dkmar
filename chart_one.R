a84e7c56a681a19a59e292c582f720f1915918de
Chloe <cjl0707@uw.edu>
2018-05-12 11:22:36 -0700

    chart-one v9

--- chart_one.R
+++ chart_one.R
@@ -10,11 +10,11 @@ source("summary.R")
 # Function for drawing the chart 
 chart_one_function <- function(data, x, y, fill_y) {
   # setting the color for the chart
-  color_select <- c(c(rgb(254/255, 67/255, 101/255), 
-                      rgb(252/255, 157/255, 154/255),
-                      rgb(249/255, 205/255, 173/255),
-                      rgb(248/255, 202/255, 0/255),
-                      rgb(131/255, 175/255, 155/255)))
+  color_select <- c(c(rgb(254 / 255, 67 / 255, 101 / 255),
+                      rgb(252 / 255, 157 / 255, 154 / 255),
+                      rgb(249 / 255, 205 / 255, 173 / 255),
+                      rgb(248 / 255, 202 / 255, 0 / 255),
+                      rgb(131 / 255, 175 / 255, 155 / 255)))
   # making the chart using ggplot2 and ggplotly
   info <- paste0("Number of Students: ", data[[y]])
   chart <- ggplot() + theme_bw() +
@@ -29,12 +29,9 @@ chart_one_function <- function(data, x, y, fill_y) {
                                            colour = "black"),
           axis.title.y = element_text(angle = 0, vjust = 0.5),
           plot.title = element_text(face = "bold", hjust = 0.5)) +
-    labs(x =gsub("_", " ", x),
+    labs(x = gsub("_", " ", x),
          y = gsub("_", " ", y),
          fill = gsub("_", " ", fill_y)) +
     ggtitle(paste0(gsub("_", " ", fill_y), " by ", gsub("_", " ", x)))
   return(ggplotly(chart, tooltip = c("info")))
 }
-
-chart_one_function(sorted_table, "class_standing",
-                   "number_of_students", "coding_experience")
