429c76f41b2101f13ff4bcd13747692921712b1d
Daniel Mar <daniel.k.mar@outlook.com>
2018-05-13 20:04:40 -0700

    Passing lintrgit add chart_two.R

--- chart_two.R
+++ chart_two.R
@@ -1,7 +1,5 @@
 library("dplyr")
-# Ignore the dev ggplot2 recommendation
-# Install cran version if necessary:
-# devtools::install_github('cran/ggplot2')
+# Please ignore the dev ggplot2 recommendation
 library("ggplot2")
 library("ggmosaic")
 library("plotly")
@@ -9,19 +7,19 @@ library("plotly")
 # Correlation between coding experience and use of git
 chart_two_function <- function(data) {
   code_xp_git <- count(data, coding_experience, using_git)
-  
+
   # Define ordering of Git experience (E.g. Expert after Intermediate)
   git_xp_levels <- c(
     "Not Specified", "Never", "Have",
     "Intermediate", "Expert"
   )
-  
+
   # Define ordering of coding experience (E.g. Lots after Moderate)
   code_xp_levels <- c(
     "Not Specified", "Never",
     "Experimented", "Moderate", "Lots"
   )
-  
+
   # Implement above orderings
   code_xp_git <- code_xp_git %>%
     mutate(
@@ -29,14 +27,14 @@ chart_two_function <- function(data) {
       coding_experience = factor(coding_experience, code_xp_levels)
     ) %>%
     arrange(coding_experience, using_git)
-  
+
   # Color scale for chart
   color_select <- c(c(rgb(254 / 255, 67 / 255, 101 / 255),
                       rgb(252 / 255, 157 / 255, 154 / 255),
                       rgb(249 / 255, 205 / 255, 173 / 255),
                       rgb(248 / 255, 202 / 255, 0 / 255),
                       rgb(131 / 255, 175 / 255, 155 / 255)))
-  
+
   ggplot(data = code_xp_git) +
     geom_mosaic(aes(
       weight = n,
@@ -44,19 +42,19 @@ chart_two_function <- function(data) {
       fill = using_git
     )) +
     scale_fill_manual(
-      guide = guide_legend(title = "Git Usage", reverse = TRUE), 
+      guide = guide_legend(title = "Git Usage", reverse = TRUE),
       values = rev(color_select)) +
-    labs(x = "Coding Experience", 
+    labs(x = "Coding Experience",
          title = "Git Usage by Coding Experience Level") +
     theme(
-      plot.title = element_text(hjust = 0.5, size = 14), 
-      plot.margin = margin(b = 20), 
+      plot.title = element_text(hjust = 0.5, size = 14),
+      plot.margin = margin(b = 20, l = 5, r = 5),
       axis.text.x = element_text(
-        angle = 45, 
-        margin = margin(t = 25), 
+        angle = 45,
+        margin = margin(t = 25),
         size = 11),
       axis.title.x = element_text(
-        margin = margin(t = -15), 
-        size = 14, 
+        margin = margin(t = -15),
+        size = 14,
         hjust = 0.5))
 }
